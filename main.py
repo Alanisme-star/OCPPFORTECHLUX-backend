00caa1e42f4ea1811a3d03a9316e08f558db2211
jmchen999 <https://github.com/jmchen999/realtime-translator-cloud.git>
2025-08-23 01:41:43 +0800

    修正

--- main.py
+++ main.py
@@ -2741,6 +2741,7 @@ def last_transactions():
 
 
 if __name__ == "__main__":
-    import os
-    port = int(os.getenv("PORT", "10000"))  # Render 會注入 PORT
+    import os, uvicorn
+    port = int(os.getenv("PORT", "10000"))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=False)
+
