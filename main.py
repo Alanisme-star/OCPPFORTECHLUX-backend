d79a3c802fa10c1a5db16e660920c2b6ba718b7b
jmchen999 <https://github.com/jmchen999/realtime-translator-cloud.git>
2025-10-08 11:38:15 +0800

    修正

--- main.py
+++ main.py
@@ -1328,7 +1328,8 @@ async def stop_transaction_by_charge_point(charge_point_id: str):
     print(f"🟢【API呼叫】發送 RemoteStopTransaction 給充電樁")
     print(f"🟢【API呼叫】即將送出 RemoteStopTransaction | charge_point_id={charge_point_id} | transaction_id={transaction_id}")
     # 送 RemoteStopTransaction（使用 Payload）
-    req = call.RemoteStopTransactionPayload(transaction_id=int(transaction_id))
+    # req = call.RemoteStopTransactionPayload(transaction_id=int(transaction_id)) 先隱藏
+    req = call.RemoteStopTransaction(transaction_id=int(transaction_id))
     resp = await cp.call(req)
     print(f"🟢【API回應】呼叫 RemoteStopTransaction 完成，resp={resp}")
 
